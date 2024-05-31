b5e282ccc75469e2e61beac7796748f6c31abf96
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-31 16:32:04 -0300

    :sparkles: instance settings class

--- ame_data_engineer_challenge/core/config.py
+++ ame_data_engineer_challenge/core/config.py
@@ -5,5 +5,9 @@ class Settings(BaseSettings):
     DB_URL: str
     SPARK_URL: str
     SPARK_DB_DRIVER: str
+    APP_NAME: str
 
-    model_config = SettingsConfigDict(".env")
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
