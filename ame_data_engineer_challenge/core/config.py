216f6542b7ec5bb93ebd5f51bc632614ad9ef700
joaooliveira247 <joaooliveira247@gmail.com>
2024-06-02 15:18:48 -0300

    :recycle: change settings consts

--- ame_data_engineer_challenge/core/config.py
+++ ame_data_engineer_challenge/core/config.py
@@ -2,10 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DB_URL: str
     SPARK_URL: str
     SPARK_DB_DRIVER: str
     APP_NAME: str
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
