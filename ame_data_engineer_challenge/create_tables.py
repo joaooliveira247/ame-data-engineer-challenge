729731043e27abb110b8b26c3bcad7b53aa9e815
joaooliveira247 <joaooliveira247@gmail.com>
2024-06-02 11:07:27 -0300

    :sparkles: add const CREATE_TABLE_COUNTRY

--- ame_data_engineer_challenge/create_tables.py
+++ ame_data_engineer_challenge/create_tables.py
@@ -2,6 +2,11 @@ CREATE_TABLE_OS: str = """
 CREATE TABLE IF NOT EXISTS operation_system(id SERIAL PRIMARY KEY, name VARCHAR(100));
 """
 
+CREATE_TABLE_COUNTRY: str = """
+CREATE TABLE IF NOT EXISTS country(id SERIAL PRIMARY KEY, name VARCHAR(100));
+"""
+
+
 def main() -> None: ...
 
 
